02494f7de81aacfddff99aa7f80359f8c29f3897
Gabriel Rezes <70073326+GabrielRezes@users.noreply.github.com>
2023-03-26 17:26:20 -0300

    feat: add seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,40 @@
+import { useEffect } from 'react';
+import Head from 'next/head';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 import Experiences from '../components/Experiences';
 import Footer from '../components/Footer';
 import FormContact from '../components/FormContact';
 import Header from '../components/Header';
 import HomeHero from '../components/HomeHero';
 import Projects from '../components/Projects';
+
 import { HomeContainer } from '../styles/pages/HomeStyles';
 
 export default function Home() {
+  useEffect(() => {
+    Aos.init({ duration: 1500 });
+  }, []);
+
   return (
     <HomeContainer>
+      <Head>
+        <title>Home | Meu portfólio</title>
+        <meta
+          name="description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:image:secure_url" content="/ogimage.png" />
+        <meta name="twitter:image" content="/ogimage.png" />
+        <meta name="twitter:image:src" content="/ogimage.png" />
+        <meta
+          property="og:description"
+          content="Sou um desenvolvedor Front-end e aqui apresento alguns projetos desenvolvidos por mim!"
+        />
+      </Head>
+
       <Header />
 
       <main className="container">
