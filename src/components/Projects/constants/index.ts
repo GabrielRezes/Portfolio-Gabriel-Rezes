f19ec0ec9057657632901a709185ae99199aac94
Gabriel Rezes <70073326+GabrielRezes@users.noreply.github.com>
2023-03-25 22:13:25 -0300

    feat: mock json projeto

---
+++ src/components/Projects/constants/index.ts
@@ -0,0 +1,9 @@
+export const PROJECTS = [
+  {
+    imgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd_aOGVvsV8lBrC9PUWCpHrvKAdjatx1yS0A&usqp=CAU',
+    title: 'Projeto 1',
+    type: 'Website',
+    slug: 'projeto-1'
+  }
+];
